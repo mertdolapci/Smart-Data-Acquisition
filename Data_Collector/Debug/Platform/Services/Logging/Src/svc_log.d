cafebe0f43e077ebbaa49134663fb894130fdd10
mertdolapci <mertdolapci@outlook.com>
2026-06-06 23:02:09 +0300

    Clean STM32CubeIDE project metadata and ignore generated files

--- Data_Collector/Debug/Platform/Services/Logging/Src/svc_log.d
+++
@@ -1,6 +0,0 @@
-Platform/Services/Logging/Src/svc_log.o: \
- ../Platform/Services/Logging/Src/svc_log.c \
- C:/STM32F7_WS/Data_Collector/Platform/Services/Logging/Inc/svc_log.h \
- C:/STM32F7_WS/Data_Collector/Platform/BSP/Inc/bsp_uart.h
-C:/STM32F7_WS/Data_Collector/Platform/Services/Logging/Inc/svc_log.h:
-C:/STM32F7_WS/Data_Collector/Platform/BSP/Inc/bsp_uart.h:
